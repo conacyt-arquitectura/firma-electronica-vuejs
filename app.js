a6782e6166c2aebb6ff69557dbd10921ef9ab780
Erik Valdivieso <erik.valdivieso@cns-ipicyt.mx>
2020-03-05 19:49:31 -0600

    pruebas de varios certificados

--- app.js
+++ app.js
@@ -1,60 +1,88 @@
 var express = require('express');
 var app = express();
 
-var path = require('path');
-var root = path.dirname(require.main.filename);
+//var path = require('path');
+//var root = path.dirname(require.main.filename);
 
 var fs = require('fs');
-
 var jseu = require('js-encoding-utils');
-
-
-const { Certificate, PrivateKey } = require('@fidm/x509')
-
+var { Certificate, PrivateKey } = require('@fidm/x509');
 var keyutils = require('js-crypto-key-utils');
 
-const certObj = fs.readFileSync('/home/usuario/tmp1/cert/GONM430818HP3/gonm430818hp3.cer');
-const keyObj = new keyutils.Key('der', fs.readFileSync('/home/usuario/tmp1/cert/GONM430818HP3/GONM430818HP3.key'));
-const yourPassphrase = '12345678a';
+const algoritmo = 'sha512';
 
-async function prueba(pemCert) {
-    const isEncrypted = keyObj.isEncrypted;
+async function prueba(cert, key, pass) {
+    let keyObj;
+    let certObj;
 
-    if (isEncrypted) {
-        //console.log("isEncrypted")
+    try {
+      keyObj = new keyutils.Key('der', fs.readFileSync(key));
+      if (!keyObj.isEncrypted || !keyObj.isPrivate) {
+        console.error('El archivo de la llave no es una llave privada');
+        return false;
+      }
+  } catch (e) {
+      console.error('No se pudo cargar la llave');
+      return false;
+    }
 
-        await keyObj.decrypt(yourPassphrase);
+    try {
+      certObj = jseu.formatter.binToPem(fs.readFileSync(cert), 'certificate');
+    } catch(e) {
+      console.error('No se pudo cargar el certificado');
+      return false;
+    }
 
-        let privateKeyPem = await keyObj.export('pem');
+    let privateKeyPem;
 
-        //console.log('Private: ' + privateKeyPem);
+    if (keyObj.isEncrypted) {
+      try {
+        await keyObj.decrypt(pass);
+        privateKeyPem = await keyObj.export('pem');
+      } catch(e) {
+        console.error('No se pudo desencriptar la llave, el password no parece ser corecto');
+        return false;
+      }
+    }
 
-        const privateKey = PrivateKey.fromPEM(privateKeyPem);
+    const data = Buffer.allocUnsafe(100); //'Esto es una prueba';
+    let privateKey;
 
-        let pem;
+    try{
+      let privateKeyPem = await keyObj.export('pem');
+      privateKey = PrivateKey.fromPEM(privateKeyPem);
+    } catch(e) {
+      console.error('No se pudo generar el objeto de la llave');
+      return false;
+    }
 
-        if(keyObj.isPrivate) pem = await keyObj.export('pem', {outputPublic: true});
+    let ed25519Cert;
 
-        //console.log('Public: ' + pem);
+    try {
+      ed25519Cert = Certificate.fromPEM(certObj);
+    } catch(e) {
+      console.error('No se pudo generar el objeto del certificado');
+      return false;
+    }
 
+    let signature;
 
-        const data = Buffer.allocUnsafe(100); //'Esto es una prueba';
+    try {
+      signature = privateKey.sign(data, algoritmo)
+    } catch(e) {
+      console.error('No se pudo cifrar la información');
+      return false;
+    }
 
-        const signature = privateKey.sign(data, 'sha512')
-
-        const ed25519Cert = Certificate.fromPEM(pemCert);
-
-        //console.log('json: ' + ed25519Cert.subject);
-
-        //console.log('CERT: ' + pemCert);
-
-        if (ed25519Cert.publicKey.verify(data, signature, 'sha512')) {
-          console.error('Es válido');
-        } else {
-          console.error('No es válido');
-        }
-
-
+    try {
+      if(ed25519Cert.publicKey.verify(data, signature, algoritmo)) {
+        console.log('Válido');
+      } else {
+        console.error('Inválido');
+      }
+    } catch(e) {
+      console.log('No se pudo hacer la verificación');
+      return false;
     }
 
 }
@@ -64,14 +92,10 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-    try {
-        const pemCert = jseu.formatter.binToPem(certObj, 'certificate');
-
-        prueba(pemCert);
-    } catch(e) {
-        console.error(e);
-    }
-
+    prueba('/home/usuario/tmp1/cert/GONM430818HP3/gonm430818hp3.cer', '/home/usuario/tmp1/cert/GONM430818HP3/GONM430818HP3.key', '12345678a'); //ok
+    prueba('/home/usuario/tmp1/cert/GONM430818HP3/gonm430818hp3.cer', '/home/usuario/tmp1/cert/GONM430818HP3/GONM430818HP3.key', '12345678a11'); //not ok
+    prueba('/home/usuario/tmp1/cert/GONM430818HP3/gonm430818hp3.cer', '/home/usuario/tmp1/cert/GUGA6307106T8/GUGA6307106T8.key', '12345678a11'); //not ok
+    prueba('/home/usuario/tmp1/cert/GUGA6307106T8/guga6307106t8.cer', '/home/usuario/tmp1/cert/GONM430818HP3/GONM430818HP3.key', '12345678a'); //not ok
 
     console.log('Example app listening on port 3000!');
 });
